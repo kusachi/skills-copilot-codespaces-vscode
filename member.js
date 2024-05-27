c99210530994ec5bacb545363c3d2baa9ecb550c
kusachi <kiyoshi@kusachi.com>
2024-05-27 08:58:18 +0000

    Copilot second commit

---
+++ member.js
@@ -0,0 +1,17 @@
+function skillsMember() {
+    var skills = [
+        { skill: 'HTML', level: 10 },
+        { skill: 'CSS', level: 8 },
+        { skill: 'JS', level: 9 },
+        { skill: 'React', level: 7 },
+        { skill: 'Node', level: 8 },
+        { skill: 'Python', level: 6 },
+        { skill: 'Java', level: 7 },
+        { skill: 'C', level: 6 },
+        { skill: 'C++', level: 6 },
+        { skill: 'C#', level: 6 },
+        { skill: 'PHP', level: 6 }
+    ];
+
+    return skills;
+}
